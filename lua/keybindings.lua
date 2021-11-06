74af01caf2e918c0a17aab33adcc8899a371bbab
kar9222 <karchua922@gmail.com>
2021-11-06 16:54:12 +1100

    Change keybinds of scroll_term

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -439,18 +439,18 @@ vimp.tnoremap('<f10>L', "<cmd>lua write_to_log('full')<CR>")
 local function scroll_term(cmd)
   vim.cmd('call g:neoterm.instances[g:neoterm.last_active].normal("' .. cmd .. '")')
 end
-vimp.nnoremap('<c-m-g>',  function () scroll_term('gg') end)
-vimp.inoremap('<c-m-g>',  function () scroll_term('gg') end)
-vimp.nnoremap('<m-s-f4>', function () scroll_term('G') end)  -- AHKREMAP <c-G>
-vimp.inoremap('<m-s-f4>', function () scroll_term('G') end)  -- AHKREMAP <c-G>
-vimp.nnoremap('<c-m-u>',  function () scroll_term("\\<c-u>") end)
-vimp.inoremap('<c-m-u>',  function () scroll_term("\\<c-u>") end)
-vimp.nnoremap('<c-m-d>',  function () scroll_term("\\<c-d>") end)
-vimp.inoremap('<c-m-d>',  function () scroll_term("\\<c-d>") end)
-vimp.nnoremap('<c-m-b>',  function () scroll_term("\\<c-b>") end)
-vimp.inoremap('<c-m-b>',  function () scroll_term("\\<c-b>") end)
-vimp.nnoremap('<c-m-f>',  function () scroll_term("\\<c-f>") end)
-vimp.inoremap('<c-m-f>',  function () scroll_term("\\<c-f>") end)
+vimp.nnoremap('<c-m-h>', function () scroll_term('gg') end)
+vimp.inoremap('<c-m-h>', function () scroll_term('gg') end)
+vimp.nnoremap('<c-m-w>', function () scroll_term('G') end)
+vimp.inoremap('<c-m-w>', function () scroll_term('G') end)
+vimp.nnoremap('<c-m-u>', function () scroll_term("\\<c-u>") end)
+vimp.inoremap('<c-m-u>', function () scroll_term("\\<c-u>") end)
+vimp.nnoremap('<c-m-d>', function () scroll_term("\\<c-d>") end)
+vimp.inoremap('<c-m-d>', function () scroll_term("\\<c-d>") end)
+vimp.nnoremap('<c-m-b>', function () scroll_term("\\<c-b>") end)
+vimp.inoremap('<c-m-b>', function () scroll_term("\\<c-b>") end)
+vimp.nnoremap('<c-m-f>', function () scroll_term("\\<c-f>") end)
+vimp.inoremap('<c-m-f>', function () scroll_term("\\<c-f>") end)
 
 
 -- Identify the syntax highlighting group used at the cursor
