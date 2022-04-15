419f1c83a76c43ec0d1fb9e0d04786d07640c58f
kar9222 <karchua922@gmail.com>
2022-04-15 15:51:21 +1000

    Update toggleterm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -55,11 +55,13 @@ toggleterm.setup({
         width = 125,
         height = 40,
         winblend = 3,
-        highlights = {
-            background = "Normal",
-            border = "ToggleTermBorder",
-        }
     },
+    highlights = {
+        -- highlights which map to a highlight group name and a table of it's values
+        -- NOTE: this is only a subset of values, any group placed here will be set for the terminal window split
+        NormalFloat = { link = 'Normal' },
+        FloatBorder = { link = 'ToggleTermBorder' },  -- Custom highlight group
+    }
 })
 
 -- NOTE Source these after setup
