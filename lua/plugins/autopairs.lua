707701e7325b38b28f48d5e6fc78ca47b34a4654
kar9222 <karchua922@gmail.com>
2022-04-25 12:06:20 +1000

    Update nvim-autopairs

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -16,13 +16,7 @@ autopairs.setup{
     },
 }
 
--- local map_bs = true  -- map the <BS> key
--- local disable_filetype = { "TelescopePrompt" }
--- local ignored_next_char = string.gsub([[ [%w%%%'%[%"%.] ]],"%s+", "")
--- local enable_moveright = true
--- local enable_afterquote = true  -- add bracket pairs after quote
--- local enable_check_bracket_line = true  --- check bracket in same line
--- local check_ts = false
+-- See README for default values
 
 -- For nvim-cmp: Auto insert `(` after selecting function or method item
 cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
