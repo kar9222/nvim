03f9b46439c3a67969fef09d6c4c0e041aea305b
kar9222 <karchua922@gmail.com>
2024-03-24 21:18:03 +1100

    telescope: Improve fzf extension

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -91,10 +91,11 @@ telescope.setup({
         grep_previewer = previewers.vim_buffer_vimgrep.new,
         qflist_previewer = previewers.vim_buffer_qflist.new,
 
+        -- TODO Explore nvim-telescope/telescope-live-grep-args.nvim
         extensions = {
-          fzf = {
+          fzf = {  -- TODO
             fuzzy = true, -- false will only do exact matching
-            override_generic_sorter = false, -- override the generic sorter
+            override_generic_sorter = true, -- override the generic sorter
             override_file_sorter = true,     -- override the file sorter
             case_mode = "smart_case", -- or "ignore_case" or "respect_case"
                                       -- the default case_mode is "smart_case"
