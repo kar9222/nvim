3592835236eb234e9f3343a01a306ecbd39ed4c3
kar9222 <karchua922@gmail.com>
2024-03-29 19:04:48 +1100

    noice: Increase signature's throttle to 500ms

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -137,7 +137,7 @@ require'noice'.setup({
         enabled = true,
         trigger = true, -- Automatically show signature help when typing a trigger character from the LSP
         luasnip = true, -- Will open signature help when jumping to Luasnip insert nodes
-        throttle = 50, -- Debounce lsp signature help request by 50ms
+        throttle = 500, -- Debounce lsp signature help request by 50ms
       },
       view = nil, -- when nil, use defaults from documentation
       ---@type NoiceViewOptions
