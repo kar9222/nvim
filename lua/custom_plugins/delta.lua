94e6eb37cc10477bc2ec249d73809105996d14f9
kar9222 <karchua922@gmail.com>
2024-03-31 09:57:14 +1100

    git: Swap keys for delta's diff and gitsigns' toggle_deleted

--- lua/custom_plugins/delta.lua
+++ lua/custom_plugins/delta.lua
@@ -3,7 +3,7 @@ local delta = require('custom_plugins.delta.lua.init')
 whichkey.register({  -- NOTE Similar keybinds as gitsigns
     name = 'git delta',
     b  = {function() delta.diff_current_buf() end, 'diff preview current buffer'},
-    d  = {function() delta.diff() end,             'diff preview'},
+    z  = {function() delta.diff() end,             'diff preview'},
     ls = {function() delta.status() end,           'status with diff preview'},
     la = {function() delta.commits() end,          'commits for current directory with diff preview'},
     lb = {function() delta.bcommits() end,         'commits for current buffer with diff preview'},
--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -102,7 +102,7 @@ gitsigns.setup {
     map('n', '<leader>gf',  gs.preview_hunk_inline)
     map('n', '<leader>gg',  gs.preview_hunk)
     map('n', '<leader>gw',  gs.toggle_word_diff)
-    map('n', '<leader>gz',  gs.toggle_deleted)
+    map('n', '<leader>gd',  gs.toggle_deleted)
     map('n', '<leader>gtb', gs.toggle_current_line_blame)
     map('n', '<leader>gB',  function() gs.blame_line({true, true}) end)
     map('n', '<leader>gts', gs.toggle_signs)
