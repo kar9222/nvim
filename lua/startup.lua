133b23e289779a135e79e42d4da338d2fdea3e56
kar9222 <karchua922@gmail.com>
2024-04-02 22:22:51 +1100

    startup: Functionize start_aerial_nvimtree_repl with param nvim_tree_height_pct

--- lua/startup.lua
+++ lua/startup.lua
@@ -70,40 +70,42 @@ local is_project_dir = cwd:find('^/home/kar/project') ~= nil
 local not_mythings   = cwd:find('mythings$') == nil
 local is_project = fn.filereadable('DESCRIPTION') == 1
 
--- Start R REPL and placeholder
--- NOTE Manually start NvimTree to avoid some quirks
+-- @param nvim_tree_height_pct NvimTree's height split percentage (bottom) relative to Aerial (top)
+function start_aerial_nvimtree_repl(nvim_tree_height_pct)
+    -- Main buffer
+    vim.cmd('topleft vsp')
+    vim.g.main_buffer_win_id = fn.win_getid()
 
--- Main buffer
-vim.cmd('topleft vsp')
-vim.g.main_buffer_win_id = fn.win_getid()
+    -- Aerial window.
+    -- NOTE Setting width (file_explorer_width_julia) here doesn't work
+    -- possibly due to it being overridden by config.
+    -- Hence, use the config's width as per plugins/aerial.lua
+    vim.cmd('wincmd h')
+    vim.g.aerial_win_id = fn.win_getid()
 
--- Aerial window.
--- NOTE Setting width (file_explorer_width_julia) here doesn't work
--- possibly due to it being overridden by config.
--- Hence, use the config's width as per plugins/aerial.lua
-vim.cmd('wincmd h')
-vim.g.aerial_win_id = fn.win_getid()
+    require'aerial'.open_in_win(  -- Options are not table
+        vim.g.aerial_win_id,      -- Option is `target_win`
+        vim.g.main_buffer_win_id  -- Option is `soruce_win`
+    )
 
-require'aerial'.open_in_win(  -- Options are not table
-    vim.g.aerial_win_id,      -- Option is `target_win`
-    vim.g.main_buffer_win_id  -- Option is `soruce_win`
-)
+    -- NvimTree window (Manually start NvimTree to avoid some quirks)
+    vim.g.nvim_tree_height = fn.string(nvim_tree_height_pct * fn.winheight('%'))
+    vim.cmd('split | resize ' .. vim.g.nvim_tree_height)
+    vim.g.nvim_tree_win_id = fn.win_getid()
 
--- NvimTree window
-vim.g.nvim_tree_height = fn.string(0.6 * fn.winheight('%'))
-vim.cmd('split | resize ' .. vim.g.nvim_tree_height)
-vim.g.nvim_tree_win_id = fn.win_getid()
-
--- It opens file in window from which I last opened the tree.
--- Here, Aerial would be the "last opened" window.
--- To workaround, jump to the "main" buffer just before opening NvimTree
-vim.cmd('wincmd l')  -- To main buffer
-require'nvim-tree.api'.tree.open({ winid = vim.g.nvim_tree_win_id })
+    -- It opens file in window from which I last opened the tree.
+    -- Here, Aerial would be the "last opened" window.
+    -- To workaround, jump to the "main" buffer just before opening NvimTree
+    vim.cmd('wincmd l')  -- To main buffer
+    require'nvim-tree.api'.tree.open({ winid = vim.g.nvim_tree_win_id })
+end
 
 if is_project_dir and is_project and not_mythings then
+    start_aerial_nvimtree_repl(0.5)
     start_R_repl()
     start_placeholder()
 else
+    start_aerial_nvimtree_repl(0.8)
     vim.cmd('wincmd l')
 end
 
