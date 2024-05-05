be47fc429886a0c4f6b2d24bab9da97cd369b36e
kar9222 <karchua922@gmail.com>
2024-05-06 00:01:00 +1000

    nvim_web_devicons: Add .csl

--- lua/plugins/nvim_web_devicons.lua
+++ lua/plugins/nvim_web_devicons.lua
@@ -4,6 +4,12 @@ require('nvim-web-devicons').set_icon {
     color = '#75aadb',
     -- cterm_color = '65',
     name = 'qmd'
-  }
+  },
+  csl = {  -- Same as .bib
+    icon = "󱉟",
+    color = "#cbcb41",
+    cterm_color = "185",
+    name = "BibTeX",
+  },
 }
 
